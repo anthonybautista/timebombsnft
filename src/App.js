928079272d4d16b2495cc939e789d3fa69f41855
Anthony Bautista <anthonya.bautista@gmail.com>
2022-01-14 15:27:26 -0800

    Major updates

--- src/App.js
+++ src/App.js
@@ -1,37 +1,179 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo-large.png';
 import './App.css';
-import { useEffect } from 'react';
-import ReactDOM from 'react-dom';
+import { ethers } from 'ethers';
 import gif from "./TIME_BOMBs.gif";
-import bomb from "./bomb.png";
+import bomb1 from "./bomb1.png";
+import bomb2 from "./bomb2.png";
+import bomb3 from "./bomb3.png";
+import bomb4 from "./bomb4.png";
+import xrpant from "./xrpant.jpg";
+import snowbound from "./snowbound.jpg";
+import thevert from "./thevert.jpg";
+import twitter from "./twitter.png";
+import discord from "./discord.png";
 import three from "./3.png";
 import fourteen from "./14.png";
 import eightynine from "./89.png";
 import $ from 'jquery';
-/* import contract from './contracts/NFTCollectible.json'; */
+import NFT from './nft-abi.json';
 
 function App() {
-  /*const contractAddress = "0x355638a4eCcb777794257f22f50c289d4189F245";
-  const abi = contract.abi;*/
-
+    const NFT_ADDRESS = "0x8B4C4A3407E9a1eFF4863DA752C54e8c430274c3";
     let myBombTokenIds = [];
-
     let myBombIndex = 1;
-
     let x;
-
     let y;
+    let provider
+    let signer
+    let currentNetwork
+    const defaultNetwork = '0xa86a'
+    const { ethereum } = window;
 
-    const checkWalletIsConnected = () => { }
+    /* web3 code borrowed and modified from ftso.alexdupre.com */
+    const networks = {
+        '0xa86a': {
+            chainId: '0xa86a',
+            chainName: 'Avalanche',
+            nativeCurrency: { decimals: 18, symbol: 'AVAX' },
+            rpcUrls: ['https://api.avax.network/ext/bc/C/rpc'],
+            blockExplorerUrls: ['https://snowtrace.io'],
+        },
+    }
 
-    const connectWalletHandler = () => { }
+    async function checkWeb3() {
+        if (typeof window.ethereum !== 'undefined') {
+            console.log('Web3 provider is installed!')
+            provider = new ethers.providers.Web3Provider(window.ethereum)
+            signer = provider.getSigner()
+            try {
+                // check if already connected by getting the wallet address
+                await signer.getAddress()
+                await checkNetwork()
+                await checkAccount()
+                $("button#connect").html('Connected');
+                $("button#connect").prop('disabled', true);
+                getMinted();
+            } catch (err) {
+                console.log(err)
+            }
+        } else {
+            console.log('Please install MetaMask or another web3 provider!')
+            alert('Please install MetaMask or another web3 provider!')
+        }
+    }
+
+    async function connect() {
+        console.log("Connecting...")
+        $("p#bombError").text(``);
+
+        $("button#connect").prop('disabled', true);
+        try {
+            // connect
+            await ethereum.request({ method: 'eth_requestAccounts' });
+            await checkNetwork();
+            await checkAccount();
+            console.log("Connected.");
+            $("button#connect").html('Connected');
+            getMinted();
+        } catch (err) {
+            $("button#connect").prop('disabled', false);
+            if (err.code === 4001) {
+                // user rejected the connection
+                console.log('Please connect to MetaMask.');
+            } else {
+                console.error(err);
+            }
+            $("p#bombError").text(`${err}`);
+        }
+    }
+
+    async function checkNetwork() {
+        ethereum.on('chainChanged', handleChainChanged);
+        let chainId = await ethereum.request({ method: 'eth_chainId' })
+        if (networks[chainId] !== undefined) {
+            currentNetwork = networks[chainId]
+            //await handleChainChanged(chainId)
+        } else if (ethereum.isMetaMask) {
+            await setupNetwork(defaultNetwork)
+            if (chainId === await ethereum.request({ method: 'eth_chainId' }))
+                console.log('Please switch to one of the supported networks.')
+        } else {
+            console.log('Please switch to one of the supported networks.')
+        }
+    }
+
+    async function checkAccount() {
+        let accounts = await ethereum.request({ method: 'eth_accounts' })
+        ethereum.on('accountsChanged', handleAccountsChanged)
+
+    }
+
+    async function setupNetwork(network) {
+        try {
+            await ethereum.request({
+                method: 'wallet_switchEthereumChain',
+                params: [{ chainId: networks[network].chainId }],
+            });
+        } catch (switchError) {
+            // This error code indicates that the chain has not been added to MetaMask.
+            if (switchError.code === 4902) {
+                try {
+                    await ethereum.request({
+                        method: 'wallet_addEthereumChain',
+                        params: [networks[network]],
+                    });
+                } catch (addError) {
+                    if (addError.code === 4001) {
+                        console.log('Please approve the Avalanche network.')
+                    } else {
+                        // handle other "add" errors
+                        console.error(addError)
+                    }
+                }
+            } else if (switchError.code === 4001) {
+                // user rejected the change
+                console.log('Avalanche configuration already present.')
+            } else {
+                // handle other "switch" errors
+                console.error(switchError)
+            }
+        }
+        provider = new ethers.providers.Web3Provider(window.ethereum)
+        signer = provider.getSigner()
+    }
+
+
+    async function handleChainChanged(chainId) {
+        //window.location.reload()
+        console.log("Chain changed to " + chainId)
+        $("p#bombError").text(``);
+        currentNetwork = networks[chainId]
+        // reload provider and signer on chain change
+        provider = new ethers.providers.Web3Provider(window.ethereum)
+        signer = provider.getSigner()
+        if (currentNetwork !== undefined) {
+            // show main section
+        } else {
+            // reload or ask to change network
+            console.log('Please switch to one of the supported networks.')
+            $("p#bombError").text(`Wrong Network`);
+        }
+    }
+
+    async function handleAccountsChanged(accounts) {
+        //window.location.reload()
+        console.log('Account changed to ' + accounts)
+        if (accounts.length === 0) {
+            // MetaMask is locked or the user has not connected any accounts
+        }
+    }
 
     const mintNftHandler = () => { }
 
     const connectWalletButton = () => {
       return (
-          <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
+          <button id={'connect'} className='cta-button connect-wallet-button'>
             Connect
           </button>
       )
@@ -39,7 +181,7 @@ function App() {
 
     const myBombsButton = () => {
         return (
-            <button onClick={connectWalletHandler} id={'myBombs'} className='cta-button connect-wallet-button'>
+            <button id={'myBombs'} className='cta-button connect-wallet-button'>
                 My Bombs
             </button>
         )
@@ -53,6 +195,22 @@ function App() {
         )
     }
 
+    const gameplayButton = () => {
+        return (
+            <button id={'gameplay'} className='cta-button connect-wallet-button'>
+                Gameplay
+            </button>
+        )
+    }
+
+    const roadmapButton = () => {
+        return (
+            <button id={'roadmap'} className='cta-button connect-wallet-button'>
+                Roadmap
+            </button>
+        )
+    }
+
     const mintNftButton = () => {
       return (
           <button onClick={mintNftHandler} className='cta-button connect-wallet-button'>
@@ -62,19 +220,21 @@ function App() {
     }
 
     function returnRules() {
-        let rules = `<div class="contentBox">
-            <h3>Gameplay</h3>
+        let rules = `<div class="smallBox">
+            <h2>Gameplay</h2>
+            </div>
+            <div class="contentBox">
             <ul>
                 <li>
                     <div class="list">
-                        <img src=${bomb} class="bomb" alt="Bomb" />
+                        <img src=${bomb1} class="bomb" alt="Bomb" />
                         <span>85% of mint sales will be used to bond for TIME, and converted to wMEMO.</span>
                     </div>
                 </li>
                 <br/>
                 <li>
                     <div class="list">
-                        <img src=${bomb} class="bomb" alt="Bomb" />
+                        <img src=${bomb2} class="bomb" alt="Bomb" />
                         <span>Once the game starts, all bombs will have 24 hours to be 'reset'. Resetting your bomb
                               has a 10% chance of causing detonation.</span>
                     </div>
@@ -82,15 +242,15 @@ function App() {
                 <br/>
                 <li>
                     <div class="list">
-                        <img src=${bomb} class="bomb" alt="Bomb" />
-                        <span>Once reset, there will be a 24 hour delay until a new 24 hour timer starts. If your 24 hour
+                        <img src=${bomb3} class="bomb" alt="Bomb" />
+                        <span>Once reset, there is a 24-hour cooldown before the timer starts again. If your 24-hour
                               timer runs out, the bomb is detonated.</span>
                     </div>
                 </li>
                 <br/>
                 <li>
                     <div class="list">
-                        <img src=${bomb} class="bomb" alt="Bomb" />
+                        <img src=${bomb4} class="bomb" alt="Bomb" />
                         <span>When there are only 3 active bombs left, anyone can halt the game and the owners of the
                               remaining bombs can collect their share of the wMEMO.</span>
                     </div>
@@ -101,13 +261,45 @@ function App() {
         return rules;
     }
 
+    function returnRoadmap() {
+        let roadmap = `<div class="smallBox">
+            <h2>Roadmap</h2>
+            </div>
+            <div class="smallBox">
+                <h2>Round 1</h2>
+                <p>Round 1 will begin shortly after all 999 TIMEBOMBs have been minted.</p>
+                <p class="underlined">Proceeds will be divided as follows:</p>
+                <p>Prize Pool: 85%</p>
+                <p>Developer: 10%</p>
+                <p>Community Manager: 2.5%</p>
+                <p>Art for Round 2: 2.5%</p>
+            </div>
+            <div class="smallBox">
+                <h2>Round 2</h2>
+                <p>Round 2 artwork is already in progress. Round two will 
+                   be a collection of 3,333 unique TIMEBOMBs.</p>
+                <p>All Round 1 bombs will be eligible to participate in Round 2.</p>
+            </div>`;
+
+        return roadmap;
+    }
+
     function homeContent() {
         let collage = `<div><img src="${gif}" alt="Collage of TIMEBOMBS"/><br/></div>`;
         let mintDiv = `<div id="mint"><h3>Bombs Minted</h3><p id="bombError"></p><p><span id="bombsMinted">0</span> / 999</p>
         <div id={'mintNftButton'}><button onClick={mintNftHandler} class='cta-button connect-wallet-button'>
-        Mint NFT</button></div></div>`;
+        Mint NFT</button><p>Price: 1 AVAX</p></div></div>`;
+        return collage+mintDiv;
+    }
+
+    function gameplayContent() {
         let rules = returnRules();
-        return collage+mintDiv+rules;
+        return rules;
+    }
+
+    function roadmapContent() {
+        let roadmap = returnRoadmap();
+        return roadmap;
     }
 
     function myBombs() {
@@ -123,8 +315,8 @@ function App() {
 
         return `<div id="statusBox">
                     <div id="statusLeft">
-                        <h3>Bombs Left</h3>
                         <p id="bombsLeft">${bombsLeft}</p>
+                        <p>Bombs Left</p>
                     </div>
                     <div id="statusRight">
                         <button onClick={refreshStatus()} class='cta-button connect-wallet-button'>
@@ -145,7 +337,7 @@ function App() {
         return `
         <div id="myBombsBox">
             <h3>My Bombs</h3>
-            <p><span id="leftArrow">&larr;</span> ${index} / ${numBombs} <span id="rightArrow">&rarr;</span></p>
+            <p id="myBombNumbers"><span id="leftArrow">&larr;</span>  ${index} / ${numBombs}  <span id="rightArrow">&rarr;</span></p>
             <div><img src="${URI}" alt="TIMEBOMB #${myBombTokenIds[index-1]}"/><br/></div>
             <p>Time until timer starts: <span id="timeUntilCountdown"></span></p>
             <div id="countdownClock">24:00:00</div>
@@ -248,8 +440,24 @@ function App() {
         clearInterval(y);
     }
 
+    const getMinted = async () => {
+        const { ethereum } = window;
+
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(NFT_ADDRESS, NFT, signer);
+
+        let nMinted = await connectedContract.totalSupply();
+
+        $("span#bombsMinted").text(`${nMinted}`);
+
+        if (nMinted === 999) {
+            $("button#mint").prop('disabled', true);
+        }
+    }
+
     useEffect(() => {
-      checkWalletIsConnected();
+      checkWeb3();
     }, [])
 
     $(function() {
@@ -258,12 +466,30 @@ function App() {
             clearTimers();
             $("div#content").empty();
             $("div#content").html(homeContent());
+            getMinted();
+        })
+        $("button#gameplay").off().on("click", function(event) {
+            event.preventDefault();
+            clearTimers();
+            $("div#content").empty();
+            $("div#content").html(gameplayContent());
+        })
+        $("button#roadmap").off().on("click", function(event) {
+            event.preventDefault();
+            clearTimers();
+            $("div#content").empty();
+            $("div#content").html(roadmapContent());
         })
         $("button#myBombs").off().on("click", function(event) {
             event.preventDefault();
+            clearTimers();
             $("div#content").empty();
             $("div#content").html(myBombs());
             startCountdown();
+        })
+        $("button#connect").off().on("click", function(event) {
+            event.preventDefault();
+            connect();
         })
         $(".App").off().on('click', '#leftArrow', function () {
             if (myBombIndex > 1) {
@@ -292,12 +518,22 @@ function App() {
             <p id={'tagline'}>
               999 NFTs waiting to explode on Avalanche.
             </p>
+            <div id={'socialBar'}>
+                <img src={twitter} alt={'Twitter logo'} />
+                <img src={discord} alt={'Discord logo'} />
+            </div>
             <div id={'navBar'}>
                 <div id={'homeButton'}>
                     {homeButton()}
                 </div>
                 <div id={'myBombsButton'}>
                     {myBombsButton()}
+                </div>
+                <div id={'gameplayButton'}>
+                    {gameplayButton()}
+                </div>
+                <div id={'roadmapButton'}>
+                    {roadmapButton()}
                 </div>
                 <div id={'connectButton'}>
                     {connectWalletButton()}
@@ -317,54 +553,21 @@ function App() {
                 <div id={'mintNftButton'}>
                     {mintNftButton()}
                 </div>
-            </div>
-            <div className="contentBox">
-                <h3>Gameplay</h3>
-                <ul>
-                    <li>
-                        <div className="list">
-                            <img src={bomb} className="bomb" alt="Bomb"/>
-                            <span>85% of mint sales will be used to bond for TIME, and converted to wMEMO.</span>
-                        </div>
-                    </li>
-                    <br/>
-                    <li>
-                        <div className="list">
-                            <img src={bomb} className="bomb" alt="Bomb"/>
-                            <span>Once the game starts, all bombs will have 24 hours to be 'reset'. Resetting your bomb
-                          has a 10% chance of causing detonation.</span>
-                        </div>
-                    </li>
-                    <br/>
-                    <li>
-                        <div className="list">
-                            <img src={bomb} className="bomb" alt="Bomb"/>
-                            <span>Once reset, there will be a 24 hour delay until a new 24 hour timer starts. If your 24 hour
-                          timer runs out, the bomb is detonated.</span>
-                        </div>
-                    </li>
-                    <br/>
-                    <li>
-                        <div className="list">
-                            <img src={bomb} className="bomb" alt="Bomb"/>
-                            <span>When there are only 3 active bombs left, anyone can halt the game and the owners of the
-                          remaining bombs can collect their share of the wMEMO.</span>
-                        </div>
-                    </li>
-                </ul>
+                <p>Price: 1 AVAX</p>
             </div>
         </div>
+        <div id={'team'}>
+            <h2>Built by Apes</h2>
+            <img src={xrpant} alt={'Avax Ape 931'} />
+            <h3>xrpant - dev</h3>
+            <img src={snowbound} alt={'Avax Ape 7235'} />
+            <h3>OG Snowbound - Community Manager</h3>
+            <img src={thevert} alt={'Avax Ape 2194'} />
+            <h3>Thévert - Round 2 Art</h3>
+        </div>
+        <div id={'buffer'}></div>
     </div>
   );
 }
-
-ReactDOM.render(
-    <React.StrictMode>
-        <div id={'container'}>
-            <App />
-        </div>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
 
 export default App;
